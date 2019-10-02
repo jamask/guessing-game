506ca50ca6c61b5d4d01982ff5e6c7ea9f3f4758
Askhat <jam.askhat@gmail.com>
2019-10-03 02:03:30 +0600

    feat: solve the problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.guessNumber = 0;
     }
 
     guess() {
-
+        let guesss = this.min + Math.round((this.max - this.min) / 2);
+        this.guessNumber = guesss
+        return guesss;
     }
 
     lower() {
-
+        this.max = this.guessNumber;
     }
 
     greater() {
-
+        this.min = this.guessNumber;
     }
 }
 
